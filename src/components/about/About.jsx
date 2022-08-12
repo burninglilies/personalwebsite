360ec57e797c85faca6a166f0cfd67da6baa96ff
Lily Pham <lnp35@cornell.edu>
2022-08-12 02:16:53 -0400

    added link directories

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,9 +11,9 @@ const About = () => {
             <h4>I am a sophmore at Cornell University studying Computer Science. 
                 <br/>I am a fullstack mobile and web developer looking to land an internship for the summer of 2023.</h4>
             <div className="about-links">
-                <a href="" className="about-link"><FaLinkedin className="about-icon"/> LinkedIn</a>
-                <a href="" className="about-"><FaGithub className="about-icon"/> Github</a>
-                <a href="" className="about-"><MdPictureAsPdf className="about-icon"/> Resume</a>
+                <a href="https://www.linkedin.com/in/lily-pham-7b8593173/" className="about-link"><FaLinkedin className="about-icon"/> LinkedIn</a>
+                <a href="https://github.com/burninglilies" className="about-"><FaGithub className="about-icon"/> Github</a>
+                <a href="https://lilypham.design" className="about-"><MdPictureAsPdf className="about-icon"/> Resume</a>
             </div>
         </section>
     )
