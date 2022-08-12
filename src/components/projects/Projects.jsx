9161e1d0e47c9d01ab2c20493ddc302e6b6452d4
Lily Pham <lnp35@cornell.edu>
2022-08-12 02:19:15 -0400

    added some project details

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -10,21 +10,21 @@ const About = () => {
             <div className="projects-container">
                 <article className="project-item">
                     <img src={laptop} alt="" className="project-item-img"/>
-                    <h2>Project Title</h2>
+                    <h2>Pathfinder Visualizer</h2>
                     <h4>Description of Project</h4>
-                    <a href="" className="">BUTTON 1</a>
+                    <a href="https://github.com/burninglilies/pathfiner-project" className="">BUTTON 1</a>
+                </article>
+                <article className="project-item">
+                    <img src={laptop} alt="" className="project-item-img"/>
+                    <h2>Cornell Live</h2>
+                    <h4>Description of Project</h4>
+                    <a href="https://github.com/burninglilies/cornell-live" className="">BUTTON 1</a>
                 </article>
                 <article className="project-item">
                     <img src={laptop} alt="" className="project-item-img"/>
                     <h2>Project Title</h2>
                     <h4>Description of Project</h4>
-                    <a href="" className="">BUTTON 1</a>
-                </article>
-                <article className="project-item">
-                    <img src={laptop} alt="" className="project-item-img"/>
-                    <h2>Project Title</h2>
-                    <h4>Description of Project</h4>
-                    <a href="" className="">BUTTON 1</a>
+                    <a href="https://github.com/jjennifergu/freelance-marketplace-backend" className="">BUTTON 1</a>
                 </article>
             </div>
         </section>
