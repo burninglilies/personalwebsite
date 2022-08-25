d941130540ccede085f9db0e40552df9b2d5fb5f
Lily Pham <lnp35@cornell.edu>
2022-08-24 23:53:01 -0400

    removed skills from app for build run

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from './components/header/Header'
 import About from './components/about/About'
 import Projects from './components/projects/Projects'
-import Skills from './components/skills/Skills'
 import Footer from './components/footer/Footer'
 
 export default function App() {
