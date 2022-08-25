92992904c7144c3c67efc869c767e74ab8f1545a
Lily Pham <lnp35@cornell.edu>
2022-08-24 23:50:13 -0400

    added temp footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header'
 import About from './components/about/About'
 import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills'
+import Footer from './components/footer/Footer'
 
 export default function App() {
   return (
@@ -10,8 +11,7 @@ export default function App() {
       <Header />
       <About />
       <Projects />
-      <Skills />
-      <h1>Ur mum </h1>
+      <Footer />
     </div>
   );
 }
